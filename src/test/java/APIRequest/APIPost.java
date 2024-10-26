feb9e7edc0419f1c7e5ce49841b7780de4a08b9c
Suresh Nagur <sureshnagur70@gmail.com>
2024-10-26 16:07:06 +0530

    1st commit into local repo

---
+++ src/test/java/APIRequest/APIPost.java
@@ -0,0 +1,39 @@
+package APIRequest;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+public class APIPost {
+
+	public static void main(String[] args) {
+		
+		
+		//given - all input details
+		//when - submit the API  resource, http methods
+		//Then - validate the response
+		
+		RestAssured.basePath="https://rahulshettyacademy.com/";
+		
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body("{\r\n"
+				+ "  \"location\": {\r\n"
+				+ "    \"lat\": -38.383494,\r\n"
+				+ "    \"lng\": 33.427362\r\n"
+				+ "  },\r\n"
+				+ "  \"accuracy\": 50,\r\n"
+				+ "  \"name\": \"Suresh Nagur12\",\r\n"
+				+ "  \"phone_number\": \"(+91) 983 893 55454\",\r\n"
+				+ "  \"address\": \"29, side layout, cohen\",\r\n"
+				+ "  \"types\": [\r\n"
+				+ "    \"shoe park\",\r\n"
+				+ "    \"shop\"\r\n"
+				+ "  ],\r\n"
+				+ "  \"website\": \"http://google.com\",\r\n"
+				+ "  \"language\": \"French-IN\"\r\n"
+				+ "}\r\n"
+				+ "").when().post("maps/api/place/add/json")
+				.then().log().all().assertThat().statusCode(200);
+
+	}
+
+}
---
+++ src/test/java/APIRequest/FirstGetRequest.java
@@ -0,0 +1,19 @@
+package APIRequest;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class FirstGetRequest {
+	
+	//https://reqres.in/api/users/2
+	@Test
+	public void testcase01() {
+		
+		Response res=RestAssured.get("https://reqres.in/api/users/2");
+		System.out.println(res.asString());
+		System.out.println("Status code: "+res.getStatusCode());
+		
+	}
+
+}
